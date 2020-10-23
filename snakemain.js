b034aa32a9be115e4787eb98ca9df1a5a020bf20
hepo6479 <70782300+hepo6479@users.noreply.github.com>
2020-10-23 19:48:59 +0900

    Add files via upload

---
+++ snakemain.js
@@ -0,0 +1,49 @@
+let s;
+let scl = 20;
+let food;
+
+function setup() {
+  createCanvas(400, 400);
+  s = new Snake();
+  pickLocation();
+}
+
+function pickLocation() {
+  let cols = floor(width/scl);
+  let rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+function draw() {
+  background(51);
+  s.update();
+  s.show();
+
+  if (s.eat(food)) {
+    pickLocation();
+  }
+
+  fill(255, 0, 100)
+  rect(food.x, food.y, scl, scl);
+  frameRate(10);
+}
+
+function keyPressed() {
+  if(keyCode === UP_ARROW) {
+    s.dir(0,-1);
+  } else if(keyCode === DOWN_ARROW) {
+    s.dir(0,1);
+  } else if(keyCode === RIGHT_ARROW) {
+    s.dir(1,0);
+  } else if(keyCode === LEFT_ARROW) {
+    s.dir(-1,0);
+  }
+  //
+  // function keyPressed() {
+  //   background('yellow');
+  //   text(`${key} ${keyCode}`, 10, 40);
+  //   print(key, ' ', keyCode);
+  //   return false; // prevent default
+  // }
+}
