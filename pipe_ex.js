307ac6dc82722bfcc05f436b95fb76abad0caedb
hepo6479 <70782300+hepo6479@users.noreply.github.com>
2020-11-01 14:10:01 +0900

    Add files via upload

---
+++ pipe_ex.js
@@ -0,0 +1,50 @@
+
+
+
+function Pipe() {
+  this.top = random(height / 2);
+  this.bottom = random(height / 2);
+  this.x = width;
+  this.w = 20;
+  this.speed = 2;
+
+  this.highlight = false;
+
+  this.show = () => {
+
+    fill(255);
+    if (this.highlight) {
+      fill(255, 0, 0);
+    }
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, height - this.bottom, this.w, this.bottom);
+    image(sh, this.x, 0, 30, this.top);
+    image(sh, this.x, height - this.bottom, 30, this.bottom);
+
+  }
+
+  this.update = () => {
+    this.x -= this.speed;
+  }
+
+  this.offscreen = () => {
+    if (this.x < -this.w) {
+      return true;
+
+    } else {
+      return false;
+    }
+  }
+
+  this.hits = (bird) => {
+    if (bird.y < this.top || bird.y > height - this.bottom) {
+      if (bird.x > this.x && bird.x < this.x + this.w) {
+        this.highlight = true;
+        return true;
+      }
+    } else {
+      // this.highlight = false;
+      return false;
+    }
+  }
+}
